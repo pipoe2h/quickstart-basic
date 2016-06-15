2c77323bc50bb8a90338dc4fd1e00119d9ef0e20
aaditya sood <a@calm.io>
2016-06-15 15:47:48 +0530

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Task List 107
+                    Task List 108
                 </a>
             </div>
 
