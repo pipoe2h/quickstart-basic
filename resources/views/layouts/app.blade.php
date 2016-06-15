9337491c1e7c181f4902894641d23ca2c0e2fd5f
Sarath Kumar K <kumar.sarath588@gmail.com>
2016-06-15 14:40:35 +0530

    Task List 106

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Task List 105
+                    Task List 106
                 </a>
             </div>
 
