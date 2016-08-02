c0104b87e1032691131d081dfa6fe410e048c552
Jeevan Dongre <jeevandongre@Jeevans-MacBook-Pro.local>
2016-08-02 15:19:01 +0530

    Added Version 3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Task List v2.0
+                    Task List v3.0
                 </a>
             </div>
 
