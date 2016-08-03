5e27347ce64df50624f38322437751428a27160e
Piu <piu@ideadevice.com>
2016-08-03 16:16:28 +0530

    Removed elixir usages to test out stuff

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <!-- Fonts -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
     <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
-    <link rel="shortcut icon" type="image/x-icon" href="{{ elixir('images/favicon.ico') }}">
+    <!-- <link rel="shortcut icon" type="image/x-icon" href="{{ elixir('images/favicon.ico') }}"> -->
 
     <!-- Styles -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
@@ -32,7 +32,7 @@
             <div class="navbar-header">
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <div>{{ elixir('images/calmio-logo.svg') }}</div>
+                    <!-- <div>{{ elixir('images/calmio-logo.svg') }}</div> -->
                 </a>
             </div>
         </div>
