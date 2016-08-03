890ec1d88b211d5e6850287a9649b2f4eb49b125
Piu <piu@ideadevice.com>
2016-08-03 22:33:25 +0530

    adding back inline style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,11 +14,10 @@
 
     <!-- Styles -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-    <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
-
     <style>
         body {
             font-family: 'Lato';
+            background-color: #F6F7F7;
         }
 
         .fa-btn {
