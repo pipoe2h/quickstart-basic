542fbd3918ae34c98e81e9507368fefc45ead615
Piu <piu@ideadevice.com>
2016-08-05 12:27:19 +0530

    removed header styles, logos etc branding, renamed app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,15 +20,6 @@
             background-color: #005495;
         }
 
-        .navbar.navbar-default {
-            background-color: #0d0d0d;
-            border: none;
-        }
-
-        .navbar-header {
-            background-color: #171F26;
-        }
-
         .fa-btn {
             margin-right: 6px;
         }
@@ -37,57 +28,13 @@
             color: #D0D0D0;
         }
 
-        #logo svg {
-            width: 70px;
-            bottom: 10px;
-            position: relative;
-        }
-        .calm-background {
-            background: rgba(0,84,149,1);
-            background: -moz-linear-gradient(top, rgba(0,84,149,1) 0%, rgba(0,36,85,1) 100%);
-            background: -webkit-gradient(left top, left bottom, color-stop(0%, rgba(0,84,149,1)), color-stop(100%, rgba(0,36,85,1)));
-            background: -webkit-linear-gradient(top, rgba(0,84,149,1) 0%, rgba(0,36,85,1) 100%);
-            background: -o-linear-gradient(top, rgba(0,84,149,1) 0%, rgba(0,36,85,1) 100%);
-            background: -ms-linear-gradient(top, rgba(0,84,149,1) 0%, rgba(0,36,85,1) 100%);
-            background: linear-gradient(to bottom, rgba(0,84,149,1) 0%, rgba(0,36,85,1) 100%);
-            filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#3e5673', endColorstr='#1a242e', GradientType=0 );
-            box-shadow: rgb(10, 31, 43) 0px 0px 30px inset;
-
-            min-width: 1080px;
-            pointer-events: none;
-            position: fixed;
-            top: 0;
-            left: 50%;
-            transform: translate( -50%, 0);
-            z-index: 0;
-            width: 100%;
-
-            svg {
-                -webkit-animation: backgroundfadein 2s; /* Safari, Chrome and Opera > 12.1 */
-                -moz-animation: backgroundfadein 2s; /* Firefox < 16 */
-                -ms-animation: backgroundfadein 2s; /* Internet Explorer */
-                -o-animation: backgroundfadein 2s; /* Opera < 12.1 */
-                animation: backgroundfadein 2s;
-                opacity: 0.3;
-                width: 100%;
-            }
-        }
     </style>
 </head>
 <body id="app-layout">
     <div id="calmgb" class="calm-background"></div>
-    <nav class="navbar navbar-default">
-        <div class="container">
-            <div class="navbar-header">
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}" id="logo">
-                </a>
-            </div>
-        </div>
-    </nav>
     <div class="container">
         <div class="row text-center task-header">
-            <h2>Calm Demo App v2.0</h2>
+            <h2>Task Manager v1.0</h2>
         </div>
     </div>
     @yield('content')
@@ -95,16 +42,5 @@
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <script>
-        $(function () {
-            $.get("./calmio-logo.svg", function (data) {
-                $('#logo').html((new XMLSerializer().serializeToString(data)));
-            });
-
-            $.get('./calm_tree.svg', function (data) {
-                $('#calmgb').html((new XMLSerializer().serializeToString(data)));
-            });
-        });
-    </script>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 - Demo App</div>
+                <div class="title">Task Manager</div>
             </div>
         </div>
     </body>
