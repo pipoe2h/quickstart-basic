f59d22e681ce5bfab7c9c77bed4df32aba674971
jeevan dongre <jeevan@jeevandongre.net>
2016-09-23 18:00:09 +0530

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
     <div id="calmgb" class="calm-background"></div>
     <div class="container">
         <div class="row text-center task-header">
-            <h2>Task Manager v6.0</h2>
+            <h2>Task Manager v6.1</h2>
         </div>
     </div>
     @yield('content')
