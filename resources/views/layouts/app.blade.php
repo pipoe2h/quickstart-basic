c4b9a1eb896137746cab7e1b974d7a5809b53192
Jasnoor Gill <jasnoor@ideadevice.com>
2016-11-25 10:05:31 +0530

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
     <div id="calmgb" class="calm-background"></div>
     <div class="container">
         <div class="row text-center task-header">
-            <h2>Task Manager v9.2</h2>
+            <h2>Task Manager v9.3</h2>
         </div>
     </div>
     @yield('content')
