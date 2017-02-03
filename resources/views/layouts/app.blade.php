0f891612fc3bc8587f337d3915eac5f639428a4c
Jasnoor Gill <jasnoor.gill@nutanix.com>
2017-02-03 14:30:31 +0530

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
     <div id="calmgb" class="calm-background"></div>
     <div class="container">
         <div class="row text-center task-header">
-            <h2>Task Manager v10.1</h2>
+            <h2>Task Manager v11.1</h2>
         </div>
     </div>
     @yield('content')
